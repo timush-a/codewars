49924a74471482385c46384ccbb4d206c918fac7
Timush Alexander <50115510+timush-a@users.noreply.github.com>
2020-04-02 17:01:56 +0200

    Update pyramid_slide_down.py

--- pyramid_slide_down.py
+++ pyramid_slide_down.py
@@ -1,18 +1,25 @@
 """
-Pyramids are amazing! Both in architectural and mathematical sense. If you have a computer, you can mess with pyramids even if you are not in Egypt at the time. For example, let's consider the following problem. Imagine that you have a pyramid built of numbers, like this one here:
+Pyramids are amazing! Both in architectural and mathematical sense.
+If you have a computer, you can mess with pyramids even if you are not in Egypt at the time.
+For example, let's consider the following problem.
+Imagine that you have a pyramid built of numbers, like this one here:
 
    /3/
   \7\ 4 
  2 \4\ 6 
 8 5 \9\ 3
 Here comes the task...
-Let's say that the 'slide down' is the maximum sum of consecutive numbers from the top to the bottom of the pyramid. As you can see, the longest 'slide down' is 3 + 7 + 4 + 9 = 23
+Let's say that the 'slide down' is the maximum sum of consecutive numbers from the top to the bottom of the pyramid.
+As you can see, the longest 'slide down' is 3 + 7 + 4 + 9 = 23
 
-Your task is to write a function longestSlideDown (in ruby: longest_slide_down) that takes a pyramid representation as argument and returns its' largest 'slide down'. For example,
+Your task is to write a function longestSlideDown 
+that takes a pyramid representation as argument and returns its' largest 'slide down'. For example,
 
 longestSlideDown([[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]) => 23
 By the way...
-My tests include some extraordinarily high pyramids so as you can guess, brute-force method is a bad idea unless you have a few centuries to waste. You must come up with something more clever than that.
+My tests include some extraordinarily high pyramids so as you can guess,
+brute-force method is a bad idea unless you have a few centuries to waste.
+You must come up with something more clever than that.
 
 (c) This task is a lyrical version of the Problem 18 and/or Problem 67 on ProjectEuler.
 """
@@ -23,3 +30,4 @@ def longest_slide_down(p):
         tmp = p.pop()
         res = [tmp[i] + max(res[i],res[i+1])  for i in range(len(tmp))] 
     return res.pop()
+
