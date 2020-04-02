0ade2033261d7d755bcd2c69cac1276a48360cff
Timush Alexander <50115510+timush-a@users.noreply.github.com>
2020-04-02 17:06:54 +0200

    Update extract_the_domain_name_from_a_URL.py

--- extract_the_domain_name_from_a_URL.py
+++ extract_the_domain_name_from_a_URL.py
@@ -1,9 +1,9 @@
 import re
 
 """
-Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
-
-domain_name("http://github.com/carbonfive/raygun") == "github" 
+Write a function that when given a URL as a string,
+parses out just the domain name and returns it as a string. For example:
+domain_name("http://github.com/carbonfive/raygun") == "github"
 domain_name("http://www.zombie-bites.com") == "zombie-bites"
 domain_name("https://www.cnet.com") == "cnet"
 """
@@ -20,4 +20,3 @@ def domain_name(url):
         return http[0][3:]
     else:
         return domain[0]
-        
